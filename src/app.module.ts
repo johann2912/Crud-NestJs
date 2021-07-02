a06c8a2ff9feee97047672a7ef9863ae9950b7dc
Johann Gomez <johanngomez02@gmail.com>
2021-07-02 10:15:59 -0500

    database connection and environment creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProductModule } from './product/product.module';
+import { DatabaseModule } from './DataBase/DataBase.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, ConfigModule.forRoot(), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
